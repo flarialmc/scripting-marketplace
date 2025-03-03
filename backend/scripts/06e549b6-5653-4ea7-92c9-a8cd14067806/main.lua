13096415bedff784e4191935be24f58a37dad5e2
prathfr <89528625+prathfr@users.noreply.github.com>
2025-03-03 22:14:26 +0530

    fix index (26 -> 32)

--- backend/scripts/06e549b6-5653-4ea7-92c9-a8cd14067806/main.lua
+++ backend/scripts/06e549b6-5653-4ea7-92c9-a8cd14067806/main.lua
@@ -7,20 +7,20 @@ function onDisable()
     Notify("TPS disabled!")
 end
 
-local Ticks = {}
+Ticks = {}
 
 onEvent(EventType.onTickEvent, function()
-    local currentTime = os.clock()
-    if Ticks then
+    if (Ticks and type(Ticks) == "table") then
+        local currentTime = os.clock()
         table.insert(Ticks, currentTime)
-        while (Ticks[1] and (Ticks[1] <= currentTime - 1)) do
+        while (Ticks[1] and Ticks[1] <= currentTime - 1) do
             table.remove(Ticks, 1)
         end
     end
 end)
 
 onEvent(EventType.onRenderEvent, function()
-    if Ticks then
-        GUI.NormalRender(26, "TPS: " .. #Ticks)
+    if (Ticks and type(Ticks) == "table") then
+        GUI.NormalRender(32, "TPS: " .. #Ticks)
     end
 end)
