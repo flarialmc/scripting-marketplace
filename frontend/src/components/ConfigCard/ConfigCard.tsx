1a657feed7bdf145b32bf114e6fb8b9f66ae9a44
MBG1337x <108623200+MBG1337@users.noreply.github.com>
2025-03-17 02:05:55 +0530

    Update ConfigCard.tsx

--- frontend/src/components/ConfigCard/ConfigCard.tsx
+++ frontend/src/components/ConfigCard/ConfigCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
