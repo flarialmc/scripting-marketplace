9333862fbfc781efe71a514fddea852b427ee51b
MBG1337x <108623200+MBG1337@users.noreply.github.com>
2025-03-17 02:26:14 +0530

    Update config.ts

--- frontend/src/types/config.ts
+++ frontend/src/types/config.ts
@@ -1,11 +1,7 @@
 export interface Config {
     id: string;
     name: string;
-    imageUrl: string;  // Add this line (Make this a requirement or gay)
-    description: string;
     version: string;
     author: string;
-    downloadUrl: string;
     createdAt: string;
-    updatedAt: string;
   }
